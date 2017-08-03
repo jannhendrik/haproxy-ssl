071f286b302a42f3b3ea489f4883bdaffd87df05
Jan-Hendrik <jan.hendrik.telke@gmail.com>
2017-08-03 22:17:39 +0200

    added 443 config

--- letsencrypt/entrypoint.sh
+++ letsencrypt/entrypoint.sh
@@ -9,6 +9,12 @@ echo $HAPROXY_PORT_80_TCP_ADDR " haproxy" >> /etc/hosts
 # Generate certificates
 ./certbot-auto certonly --standalone -d www.freightpilot.eu --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de
 
+mkdir /ssl
+
+cp fullchain.pem /ssl/fullchain.pem
+cp privkey.pem /ssl/privkey.pem
+
+
 ls
 # Copy new certs and copy new nginx.conf
 
