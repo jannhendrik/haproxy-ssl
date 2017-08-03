c67da9c09f18b2211bae899ee1b53b5087fb1cb7
Jan-Hendrik <jan.hendrik.telke@gmail.com>
2017-08-03 22:44:11 +0200

    commit

--- letsencrypt/entrypoint.sh
+++ letsencrypt/entrypoint.sh
@@ -7,16 +7,14 @@ echo $HAPROXY_PORT_80_TCP_ADDR " haproxy"
 echo $HAPROXY_PORT_80_TCP_ADDR " haproxy" >> /etc/hosts
 
 # Generate certificates
-./certbot-auto certonly --standalone -d www.freightpilot.eu --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de
+./certbot-auto certonly --standalone -d www.freightpilot.eu --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de --staging
 
 mkdir /ssl
 
 cp fullchain.pem /ssl/fullchain.pem
 cp privkey.pem /ssl/privkey.pem
 
-
 ls
 # Copy new certs and copy new nginx.conf
-
 
 nginx
