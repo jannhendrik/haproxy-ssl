0635ada8f35c79d11c43f192db43d6d165f3f643
Jan-Hendrik <jan.hendrik.telke@gmail.com>
2017-08-03 23:33:55 +0200

    fixed cert location in nginx

--- letsencrypt/entrypoint.sh
+++ letsencrypt/entrypoint.sh
@@ -9,12 +9,4 @@ echo $HAPROXY_PORT_80_TCP_ADDR " haproxy" >> /etc/hosts
 # Generate certificates
 ./certbot-auto certonly --standalone -d www.freightpilot.eu --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de --staging
 
-mkdir /ssl
-
-cp fullchain.pem /ssl/fullchain.pem
-cp privkey.pem /ssl/privkey.pem
-
-ls
-# Copy new certs and copy new nginx.conf
-
 nginx
