d0b7dab5acca3e5add04271e9682f750eefb2ae8
Jan-Hendrik <jan.hendrik.telke@gmail.com>
2017-08-03 20:10:40 +0200

    activate cert generation

--- letsencrypt/entrypoint.sh
+++ letsencrypt/entrypoint.sh
@@ -8,4 +8,11 @@ echo $HAPROXY_PORT_80_TCP_ADDR " haproxy" >> /etc/hosts
 
 nginx
 
-#./certbot-auto certonly --webroot -w /usr/share/nginx/html -d www.freightpilot.eu --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de
+./certbot-auto certonly --webroot -w /usr/share/nginx/html -d www.freightpilot.eu --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de
+
+
+nginx stop
+
+ls
+
+nginx
