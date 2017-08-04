660ebc2efbfef904e414a17cb98376ce3ba10ab1
Jan-Hendrik <jan.hendrik.telke@gmail.com>
2017-08-04 12:37:41 +0200

    improved ssl sec

--- letsencrypt/entrypoint.sh
+++ letsencrypt/entrypoint.sh
@@ -6,7 +6,13 @@ echo $HAPROXY_PORT_80_TCP_ADDR " haproxy"
 echo $HAPROXY_PORT_80_TCP_ADDR " haproxy"
 echo $HAPROXY_PORT_80_TCP_ADDR " haproxy" >> /etc/hosts
 
+#Generate DH Parameters
+rm -rf /dhparam
+mkdir /dhparam
+
+openssl dhparam 2048 -out /dhparam/dhparam.pem
+
 # Generate certificates
-./certbot-auto certonly --standalone -d www.freightpilot.eu --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de --staging
+#./certbot-auto certonly --standalone -d www.freightpilot.eu --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de --staging
 
 nginx
