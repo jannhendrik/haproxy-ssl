ee07abb4050d00faf18bd5296952e9673e0eb313
Jan-Hendrik <jan.hendrik.telke@gmail.com>
2017-08-04 16:54:09 +0200

    commit

--- letsencrypt/entrypoint.sh
+++ letsencrypt/entrypoint.sh
@@ -13,6 +13,6 @@ mkdir /dhparam
 openssl dhparam 2048 -out /dhparam/dhparam.pem
 
 # Generate certificates
-./certbot-auto certonly --standalone -d www.freightpilot.eu -d freightpilot.eu -d www.freightpilot.de -d freightpilot.de --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de --staging
+./certbot-auto certonly --standalone -d www.freightpilot.eu -d www.freightpilot.de --noninteractive --agree-tos --email jan.hendrik.telke@freightpilot.de --staging
 
 nginx
