ba8ec4138b7bd4f9da4b1ccb598775eebd85bf04
Jan-Hendrik <jan.hendrik.telke@gmail.com>
2017-10-10 14:12:54 +0200

    dropped python for java + JCommander + mustache

---
+++ src/main/java/de/greenmile/haproxyssl/generation/ConfigGenerator.java
@@ -0,0 +1,25 @@
+/*
+* Copyright (C) GreenMile UG, - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+* @author jan-hendriktelke 
+* on the 10.10.17.
+*
+*/
+
+package de.greenmile.haproxyssl.generation;
+
+public class ConfigGenerator {
+
+  private String outputDir;
+  private String nginxTemplate;
+  private ConfigParameter parameter;
+
+  public ConfigGenerator() {
+  }
+
+  public void generate() {
+
+  }
+
+}
---
+++ src/main/java/de/greenmile/haproxyssl/generation/ConfigParameter.java
@@ -0,0 +1,16 @@
+/*
+* Copyright (C) GreenMile UG, - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+* @author jan-hendriktelke 
+* on the 10.10.17.
+*
+*/
+
+package de.greenmile.haproxyssl.generation;
+
+public class ConfigParameter {
+
+
+
+}
